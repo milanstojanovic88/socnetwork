1cd16d3331d2aee04ee628719af0f55b0df9b174
milan88 <milan.stojanovic88@gmail.com>
2016-06-22 20:41:23 +0200

    commit

---
+++ app/Console/Commands/Chat.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Ratchet\Http\HttpServer;
+use Ratchet\Server\IoServer;
+use Ratchet\WebSocket\WsServer;
+
+class Chat extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'chat';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Run chat server.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $server = IoServer::factory(new HttpServer(new WsServer(new \App\Chat())), 2000);
+
+        $server->run();
+    }
+}
